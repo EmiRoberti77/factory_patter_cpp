2085e1889c362a89c66254a8a14901d4c88ad415
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-06-03 05:09:19 +0100

    Initial Commit

---
+++ emicpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  emicpp
+//
+//  Created by emiliano.roberti on 03/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
